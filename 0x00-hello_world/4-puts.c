8a9a4bf3c615390861957e7228c4bb6339334b66
SireCourage001 <sirecourage@gmail.com>
2023-06-15 13:37:29 -0700

    hello_world

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,7 +2,7 @@
 
 
 /**
- * main - prints "programming is like building a multilingual
+ * main - prints "Programming is like building a multilingual
  * puzzle, followed by a new line.
  *
  * Return: Always 0.
@@ -10,7 +10,7 @@
 
 int main(void)
 {
-	puts("\"programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	
 	return (0);
 }
