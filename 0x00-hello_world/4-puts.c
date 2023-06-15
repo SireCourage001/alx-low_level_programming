769f598f9449fc763cabba58e35404809d5d6eea
SireCourage001 <sirecourage@gmail.com>
2023-06-15 11:38:38 -0700

    hello_world

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,13 +2,13 @@
 
 #include <stdio.h>
 
-/**
+/*
  * main - Entry point
  *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-	puts("|"programming is like building a multilingual puzzle");
+	puts("|"programming is like building a multilingual puzzle\n");
 	retun (0);
 }
