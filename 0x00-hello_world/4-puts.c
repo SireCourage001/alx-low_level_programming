a178fb2253721305a2b221f9b99e553ccf41327e
SireCourage001 <sirecourage@gmail.com>
2023-06-15 12:04:42 -0700

    hello_world

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,7 +3,7 @@
 #include <stdio.h>
 
 /**
- * main - Entry point
+ * main - Prints "programming is like building a multilingual puzzle, followed by a new line.
  *
  * Return: Always 0 (Success)
  */
