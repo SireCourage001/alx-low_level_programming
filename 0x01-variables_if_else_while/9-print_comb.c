b1b1f5af933dd4eb3196d7a4887e1f809a66bef8
SireCourage001 <sirecourage@gmail.com>
2023-06-16 15:21:36 -0700

    new

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -3,23 +3,23 @@
 /**
  * main - Prints all possible combinations of single-digit numbers.
  *
- * Return: Always 0(Success).
+ * Return: Always 0 (Success).
  */
 int main(void)
 {
-	int num;
+    int num;
 
-	for (num = 0; num <= 9; num++)
-	{
-		putchar((num % 10) + '0');
-		if (num == 9)
-		continue;
-		
-		putchar(',');
-		putchar(' ');
-	}
+    for (num = 0; num <= 9; num++)
+    {
+        putchar((num % 10) + '0');
+        if (num == 9)
+            continue;
 
-	putchar('\n');
+        putchar(',');
+        putchar(' ');
+    }
 
-	return (0);
+    putchar('\n');
+
+    return (0);
 }
