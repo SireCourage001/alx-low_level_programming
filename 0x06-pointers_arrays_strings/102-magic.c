bc085985a07fd162141d281247bc1af52580b39e
SireCourage001 <sirecourage@gmail.com>
2023-06-29 12:19:25 -0700

    0x06-pointers

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,21 +1,24 @@
-#include "stdio.h"
+#include <stdio.h>
 
-/**
- * infinite_add - add two string numbers
- * @n1: string number to add to n2
- * @n2: string number to add to n1
- * @r: buffer to store the sum
- * @size_r: size of buffer
- * return: pointer to sum or 0 if it does not fit in r
- */
-char *infinite_add(char *n1, char *n2, char *r, int size_r)
+int main(void)
 {
-	int len1 = 0, len2 = 0, lenS = 0;
+  int n;
+  int a[5];
+  int *p;
 
-	while (*(n1 + len1++))
-		;
-	while (*(n2 + len2++))
-		;
-	printf("%d, %d\n", len1, len2);
-	return (r);
+  a[2] = 1024;
+  p = &n;
+  /*
+   * write your line of code here...
+   * Remember:
+   * - you are not allowed to use a
+   * - you are not allowed to modify p
+   * - only one statement
+   * - you are not allowed to code anything else than this line of code
+   */
+  *(p + 5) = 98;
+  /* ...so that this prints 98\n */
+  printf("a[2] = %d\n", a[2]);
+  return (0);
 }
+
