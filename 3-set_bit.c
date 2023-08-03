0ee619e8e476557d34f2a345e32f8aa25229911e
SireCourage001 <sirecourage@gmail.com>
2023-08-03 11:55:10 -0700

    3-set_bit.c

--- 3-set_bit.c
+++ 3-set_bit.c
@@ -1,17 +1,20 @@
+#include <stdio.h>
 #include "main.h"
 
 /**
-* set_bit - sets the value of a bit to 1 at a given index,
-* @n: number,
-* @index: index,
-* Return: 1 if successful or -1 if error occured.
-*/
+ *  * set_bit - sets a bit at a given index to 1
+ *   * @n: pointer to the number to change
+ *    * @index: index of the bit to set to 1
+ *     *
+ *      * Return: 1 for success, -1 for failure
+ *       */
+int set_bit(unsigned long int *n, unsigned int index)
+
 int set_bit(unsigned long int *n, unsigned int index)
 {
-unsigned long int aux = 1 << index;
-if  (index > (sizeof((*n) * 8))
+if  (index > 63)
 return (-1);
+*n = ((1UL << index) | *n;
 
-*n = *n | aux;
 return (1);
 }
