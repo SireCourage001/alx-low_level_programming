d238f1c541b11f5f6c42c75cad7ba8606ca3710b
SireCourage001 <sirecourage@gmail.com>
2023-09-04 05:36:20 -0700

    0-read_textfile.c

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -2,45 +2,27 @@
 #include <stdlib.h>
 
 /**
-<<<<<<< HEAD
-* read_textfile - Read txt file print to STDOUT,
-* @filename: txt file being read,
-* @letters: num of letters to be read.
-* Return: w - actual num of bytes read and printed,
-* 0 when function fails or filename is NULL.
-=======
-* read_txtfile: Read txt file to print STDOUT
-* @filename: text file being read
-* @letters: num of letters to be read
-* Return: w- actual num of bytes read and printed.
-* 0 when function fails, or file_name is NULL.
->>>>>>> 359daf1a4f6765d61113e7ca8e38c1744eb107a1
-*/
+ * read_text-file - Read text-file print to STDOUT.
+ * @filename: Text file being read.
+ * @letters: Num of letters to be read.
+ * Return: w-actual num of bytes read & printed,
+ * 0 when function fails or filename is NULL.
+ */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-char *buf;
-ssize_t fd;
-ssize_t w;
-ssize_t t;
-<<<<<<< HEAD
-=======
+	char *buf;
+	ssize_t fd;
+	ssize_t w;
+	ssize_t t;
 
->>>>>>> 359daf1a4f6765d61113e7ca8e38c1744eb107a1
-fd = open(filename, O_RDONLY);
-if (fd == -1)
-return (0);
-buf = malloc(sizeof(char) * letters);
-t = read(fd, buf, letters);
-w = write(STDOUT_FILENO, buf, t);
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+		return (0);
+	buf = malloc(sizeof(char) * letters);
+	t = read(fd, buf, letters);
+	w = write(STDOUT_FILENO, buf, t);
 
-free(buf);
-close(fd);
-<<<<<<< HEAD
-
-return (w);
+	free(buf);
+	close(fd);
+	return(w);
 }
-=======
-return (w);
-}
-
->>>>>>> 359daf1a4f6765d61113e7ca8e38c1744eb107a1
