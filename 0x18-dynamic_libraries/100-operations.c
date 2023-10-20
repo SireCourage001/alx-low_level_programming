9245318e1e8f79260e650c62543ec8b2ca95d754
SireCourage001 <sirecourage@gmail.com>
2023-10-20 04:42:52 -0700

    done

---
+++ 0x18-dynamic_libraries/100-operations.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+a = random.randint(-111, 111)
+b = random.randint(-111, 111)
+print("{} + {} = {}".format(a, b, cops.add(a, b)))
+print("{} - {} = {}".format(a, b, cops.sub(a, b)))
+print("{} x {} = {}".format(a, b, cops.mul(a, b)))
+print("{} / {} = {}".format(a, b, cops.div(a, b)))
+print("{} % {} = {}".format(a, b, cops.mod(a, b)))
